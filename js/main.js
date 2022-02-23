b3a7afcd34ed25ee1955e6a7e821b840175a7f25
mike <mls.mikesmith@gmail.com>
2022-02-23 13:17:48 -0600

    all done i think

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const api_url = 'https://api.adviceslip.com/advice'
+
+function getAdvice() {
+  fetch(api_url).then((response) => {
+    if(response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE NOT OK");
+    }
+  })
+  .then(function (data) {
+    document.getElementById('advice_id').innerHTML = data.slip.id;
+    document.getElementById('advice').innerHTML = data.slip.advice;
+  })
+}
+
+document.querySelector('.die-container').addEventListener('click', () => {
+  reload = location.reload();
+});
+
+getAdvice();
